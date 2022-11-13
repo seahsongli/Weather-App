fb8568cf873f8f68a5926eaec0c20417a837553e
Philedo <seahsongli@gmail.com>
2022-11-13 08:59:12 +0800

    Moved icons directory to dist directory.

--- src/js/API.js
+++ src/js/API.js
@@ -1,9 +1,9 @@
 export async function fetchForecastWeatherData(city = "Singapore") {
     try{
-        let listOfForecastedDetails = [];
-        let response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=0e5e41d3a0b1d9cfee583d3275dd1049&units=metric`)
+        const listOfForecastedDetails = [];
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=0e5e41d3a0b1d9cfee583d3275dd1049&units=metric`)
         console.log(response);
-        let data = await response.json();
+        const data = await response.json();
         listOfForecastedDetails.push(data.list[0], data.list[7], data.list[15], data.list[23], data.list[31], data.list[39]);
         return listOfForecastedDetails;
     }
--- src/js/DOM.js
+++ src/js/DOM.js
@@ -1,17 +1,18 @@
 import * as APIfxn from "./API.js";
+
 function updateDateTime(timezone = "Singapore"){
     const currentDateTime = new Date()
-    let timeDisplayed = document.querySelector("#currentTime");
+    const timeDisplayed = document.querySelector("#currentTime");
     timeDisplayed.innerHTML= currentDateTime.toLocaleString("en-US", {timeZone: timezone}) 
 }
 setInterval(updateDateTime, 1000);
 
-let form = document.querySelector("form");
+const form = document.querySelector("form");
 form.addEventListener("submit",async(e)=>{
     e.preventDefault();
-    let currentLocation = document.querySelector("#currentLocation");
-    let searchLocation = document.querySelector(".searchLocation");
-    let error = document.querySelector(".error");
+    const currentLocation = document.querySelector("#currentLocation");
+    const searchLocation = document.querySelector(".searchLocation");
+    const error = document.querySelector(".error");
     const response = await APIfxn.fetchForecastWeatherData(searchLocation.value);
     if (response !== undefined){
         error.style.display = "none";
@@ -28,16 +29,16 @@ form.addEventListener("submit",async(e)=>{
 function getForecastedDays(date){
     const daysInWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
     const d = new Date(date);
-    let day = daysInWeek[d.getDay()];
+    const day = daysInWeek[d.getDay()];
     return day;
 }
 
 function setForecastedDays(){
-    let forecastDays = Array.from(document.querySelectorAll("#day"));
+    const forecastDays = Array.from(document.querySelectorAll("#day"));
     for (let i=0;i<forecastDays.length;i++){
-        var someDate = new Date();
-        var numberOfDaysToAdd = i;
-        var result = someDate.setDate(someDate.getDate() + numberOfDaysToAdd);
+        const someDate = new Date();
+        const numberOfDaysToAdd = i;
+        const result = someDate.setDate(someDate.getDate() + numberOfDaysToAdd);
         console.log(result);
         forecastDays[i].textContent = getForecastedDays(result);
     }
@@ -46,35 +47,35 @@ function setForecastedDays(){
 async function getForecastedWeather(city){
     const listOfForecastedDetails = await APIfxn.fetchForecastWeatherData(city);
     console.log(listOfForecastedDetails);
-    let listOfWeather = [];
-    for (let key in listOfForecastedDetails){
+    const listOfWeather = [];
+    for (const key in listOfForecastedDetails){
         listOfWeather.push(listOfForecastedDetails[key].weather[0].main);
     }
     return listOfWeather;
 }
 
 async function setForecastedWeather(city){
-    let listOfWeather = await getForecastedWeather(city);
+    const listOfWeather = await getForecastedWeather(city);
     console.log(listOfWeather);
-    let weatherIcons = Array.from(document.querySelectorAll(".weatherIcon"));
+    const weatherIcons = Array.from(document.querySelectorAll(".weatherIcon"));
     for (let i=0;i<weatherIcons.length;i++){
         if (listOfWeather[i] == "Rain"){
-            weatherIcons[i].src = "../src/icons/cloud-lightning.svg";
+            weatherIcons[i].src = "../dist/icons/cloud-lightning.svg";
         }
         else if (listOfWeather[i] == "Clouds"){
-            weatherIcons[i].src = "../src/icons/cloud.svg";
+            weatherIcons[i].src = "../dist/icons/cloud.svg";
         }
         else if (listOfWeather[i] == "Sun"){
-            weatherIcons[i].src = "../src/icons/sun.svg";
+            weatherIcons[i].src = "../dist/icons/sun.svg";
         }
     }
 }
 
 async function getForecastedTemperature(city){
     const listOfForecastedDetails = await APIfxn.fetchForecastWeatherData(city);
-    let listOfTemperatures = [];
+    const listOfTemperatures = [];
     console.log(listOfForecastedDetails);
-    for (let key in listOfForecastedDetails){
+    for (const key in listOfForecastedDetails){
         
         listOfTemperatures.push(listOfForecastedDetails[key].main.temp);
     }
@@ -82,11 +83,11 @@ async function getForecastedTemperature(city){
 }
 
 async function setForecastedTemperature(city){
-    let listOfTemperatures = await getForecastedTemperature(city);
+    const listOfTemperatures = await getForecastedTemperature(city);
     console.log(listOfTemperatures);
-    let temp = Array.from(document.querySelectorAll("#temp"));
+    const temp = Array.from(document.querySelectorAll("#temp"));
     for (let i=0;i<temp.length;i++){
-        temp[i].textContent = Math.round(listOfTemperatures[i]*10)/10 + "°";
+        temp[i].textContent = `${Math.round(listOfTemperatures[i]*10)/10  }°`;
     }
 }
 
